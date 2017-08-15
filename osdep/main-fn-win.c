7ab2c1a6b1123813333f2a56d7b24e5f02f6a433
Avi Halachmi (:avih) <avihpit@yahoo.com>
2017-08-15 23:41:12 +0300

    win32: test for msys2/cygwin pty and vt size (no-op WIP)

--- osdep/main-fn-win.c
+++ osdep/main-fn-win.c
@@ -41,9 +41,62 @@ static void microsoft_nonsense(void)
         pSetSearchPathMode(BASE_SEARCH_PATH_ENABLE_SAFE_SEARCHMODE);
 }
 
+// Cygwin/msys2 pty is a pipe with the following format (H: hex-digit, N: 0-9):
+// '\{cygwin,msys}-HHHHHHHHHHHHHHHH-ptyN-{from,to}-master'
+static bool cyg_isatty(int fd)
+{
+    HANDLE h = (HANDLE)_get_osfhandle(fd);
+    if ((h == INVALID_HANDLE_VALUE) || (GetFileType(h) != FILE_TYPE_PIPE))
+        return false;
+
+    UINT infosize = sizeof(FILE_NAME_INFO) + sizeof(WCHAR) * MAX_PATH;
+    FILE_NAME_INFO *info = malloc(infosize + sizeof(WCHAR));  // +1 for '\0'
+    if (!info)
+        return false;
+
+    bool rv = false;
+    if (GetFileInformationByHandleEx(h, FileNameInfo, info, infosize)) {
+        WCHAR *n = info->FileName;
+        // not terminated https://msdn.microsoft.com/en-us/library/ee791145.aspx
+        n[info->FileNameLength] = 0;  // we allocated for +1 WCHAR even if full
+
+        rv = ((wcsstr(n, L"\\msys-") == n) || (wcsstr(n, L"\\cygwin-") == n)) &&
+             wcsstr(n, L"-pty") &&
+             (wcsstr(n, L"-from-master") || wcsstr(n, L"-to-master"));
+    }
+    free(info);
+    return rv;
+}
+
+// returns false on failure, needs both stdin and stdout connected to the pty.
+// also needs the terminal in non line mode, which has to be set in advance,
+// e.g. by `stty -icanon` (and restore it later with `stty icanon`), otherwise
+// scanf will hang till the user presses enter (and then continue correctly).
+// based on resize.c of the xterm package: http://invisible-island.net/xterm/
+static bool vt_getsize(int *rows, int *cols) {
+    *rows = 24; *cols = 80;
+    return 1;
+    // disabled for now. too annoyignn to press enter if the term is not set up.
+
+    // save cursor, disable margin(?), move cursor, request pos, restore cursor
+    printf("\e7\e[r\e[9999;9999H\e[6n\e8"); fflush(stdout);
+    return 2 == scanf("\e[%d;%dR", rows, cols);  // expected reply format
+}
+
 int main(int argc_, char **argv_)
 {
     microsoft_nonsense();
+
+    // doesn't print anything for mpv.{com,exe} in cmd.exe, but it's NO, NO, NO
+    printf("cyg_isatty stdin:%d  stdout:%d  stderr:%d\n",
+            cyg_isatty(0), cyg_isatty(1), cyg_isatty(2));
+
+    int rows = -1, cols = -1;
+    if (cyg_isatty(0) && cyg_isatty(1) && vt_getsize(&rows, &cols)) {
+        printf("VT terminal size: rows: %d, cols: %d.\n", rows, cols);
+    } else {
+        printf("Not VT or cannot read terminal size.\n");
+    }
 
     // If started from the console wrapper (see osdep/win32-console-wrapper.c),
     // attach to the console and set up the standard IO handles
