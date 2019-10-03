d1d69c50f5fbae2118c47a38f5f254b208590681
Avi Halachmi (:avih) <avihpit@yahoo.com>
2019-10-03 23:20:26 +0300

    js: WIP: quickjs: support log (still no script loaded)

--- player/qjavascript.c
+++ player/qjavascript.c
@@ -70,9 +70,163 @@ static void set_last_error(struct script_ctx *ctx, bool iserr, const char *str)
     ctx->last_error_str = talloc_strdup_append(ctx->last_error_str, str);
 }
 
-static int s_init_js(JSContext *J, struct script_ctx *ctx) {
+#define MAYBESTR(s) ((s) ? (s) : "<N/A>")
+
+#define JOKV(v) (!JS_IsException(v))
+#define JOKI(i) ((i) >= 0)
+#define JOKS(s) ((s) != (char *)0)
+
+#define JIFV(j, var, fcall, statement) \
+    if (JOKV(var = (fcall))) { \
+        statement; \
+        JS_FreeValue(j, var); \
+    }
+
+#define JIFS(j, var, fcall, statement) \
+    if (JOKS(var = (fcall))) { \
+        statement; \
+        JS_FreeCString(j, var); \
+    }
+
+#define JEX_BEGIN \
+    int jerr = 0;
+
+#define JEX_BEGIN_TA(tavar) \
+    int jerr = 0; \
+    void *tavar = talloc_new(NULL);
+
+#define JTHROW      do { jerr = 1; goto done; } while (0)
+#define JTRYV(expr) do { if (!JOKV(expr)) JTHROW; } while (0)
+#define JTRYI(expr) do { if (!JOKI(expr)) JTHROW; } while (0)
+#define JTRYS(expr) do { if (!JOKS(expr)) JTHROW; } while (0)
+
+#define JEX_END_ONERR(rv) \
+done: \
+    if (jerr) \
+        return (rv);
+
+#define JEX_END_TA_ONERR(tavar, rv) \
+done: \
+    talloc_free(tavar); \
+    if (jerr) \
+        return (rv);
+
+#define appendf(ptr, ...) \
+    do {(*(ptr)) = talloc_asprintf_append_buffer(*(ptr), __VA_ARGS__);} while(0)
+
+static void jerr_to_last_error(struct script_ctx *ctx, JSContext *J)
+{
+    JSValue exception_val, val;
+
+    set_last_error(ctx, 1, "unknown JavaScript error");
+    if (!JOKV(exception_val = JS_GetException(J)))
+        return;
+
+    const char *s;
+    BOOL is_error, got_stack = 0;
+    char *msg = talloc_strdup(NULL, "");
+
+    is_error = JS_IsError(J, exception_val);
+    if (!is_error)
+        appendf(&msg, "Throw: ");
+
+    s = JS_ToCString(J, exception_val);
+    appendf(&msg, "%s", MAYBESTR(s));
+    if (s)
+        JS_FreeCString(J, s);
+
+    if (is_error) {
+        if (JOKV(val = JS_GetPropertyStr(J, exception_val, "stack"))) {
+            if (!JS_IsUndefined(val) && JOKS(s = JS_ToCString(J, val))) {
+                appendf(&msg, "\n%s", s);
+                got_stack = 1;
+                JS_FreeCString(J, s);
+            }
+            JS_FreeValue(J, val);
+        }
+    }
+    if (!got_stack)
+        appendf(&msg, "\n    <N/A>\n");
+
+    set_last_error(ctx, 1, msg);
+    talloc_free(msg);
+    JS_FreeValue(J, exception_val);
+}
+
+static JSValue script_log(JSContext *J, JSValueConst this_val, int argc, JSValueConst *argv)
+{
+    JSValue rv = JS_EXCEPTION;
+    const char *level = JS_ToCString(J, argv[0]);
+    int msgl = level ? mp_msg_find_level(level) : -1;
+
+    if (msgl >= 0) {
+        struct mp_log *log = jctx(J)->log;
+        for (int i = 1; i < argc; i++) {
+            const char *s = JS_ToCString(J, argv[i]);
+            mp_msg(log, msgl, (i == 1 ? "%s" : " %s"), MAYBESTR(s));
+            if (s)
+                JS_FreeCString(J, s);
+        }
+        mp_msg(log, msgl, "\n");
+        rv = JS_TRUE;
+    } else {
+        JS_ThrowReferenceError(J, "bad log level: %s", MAYBESTR(level));
+    }
+
+    if (level)
+        JS_FreeCString(J, level);
+    return rv;
+}
+
+static const JSCFunctionListEntry mp_funcs[] = {
+    JS_CFUNC_DEF("log", 1, script_log ),
+};
+
+static int s_init_js(struct script_ctx *ctx, JSContext *J) {
+    JEX_BEGIN;
+//    static const char *prog = "function x1() { log('hello') }; x1()";
+    static const char *prog = "\
+//    throw(1);\n\
+    log('info', null, Object.create(null, {}), undefined, {a:1}, [2, 3], 'str', {toString: undefined}, 4 );\n\
+    log('warn', 1, 2, 3, 'str');\n\
+    log('fatal', 1, 2, 3, 'str');\n\
+    log('foo', 1, 2, 3, 'str');\n\
+    // function x1() {\n\
+    //     var a=1;\n\
+    //     xlog('hello');\n\
+    // };\n\
+    // function x2() {\n\
+    //     var lala=12;\n\
+    //     x1();\n\
+    // };\n\
+    // throw(1);\n\
+    // throw(Error(\n\
+    //     {\n\
+    //         toString: function() { \n\
+    //             return lalala;\n\
+    //         }\n\
+    //     }\n\
+    // ));\n\
+    // x2();\n\
+    ";
     JS_SetContextOpaque(J, ctx);
-    return 0;
+
+    JSValue g;
+    JTRYV(g = JS_GetGlobalObject(J));
+    JS_SetPropertyFunctionList(J, g, mp_funcs, 1);  // void, impossible OOM?
+    JS_FreeValue(J, g);
+
+    //JTRY(fn = JS_NewCFunction(J, script_log, "log", 1), "failed to insert log fn");
+    //JS_SetPropertyStr(J, global_obj, "log", fn);
+    JTRYV(JS_Eval(J, prog, strlen(prog), "test-prog", JS_EVAL_TYPE_GLOBAL));
+
+    int msgl = mp_msg_find_level("warn");
+    mp_msg(jctx(J)->log, msgl, "%s\n", "done1");
+
+done:
+    if (jerr)
+        jerr_to_last_error(ctx, J);
+    return jerr;
 }
 
 static int s_load_javascript(struct mpv_handle *client, const char *fname)
@@ -91,7 +245,7 @@ static int s_load_javascript(struct mpv_handle *client, const char *fname)
     JSContext *J = NULL;
     if (!(jrt = JS_NewRuntime()) ||
         !(J = JS_NewContext(jrt)) ||
-        s_init_js(J, ctx))
+        s_init_js(ctx, J))
     {
         goto error_out;
     }
