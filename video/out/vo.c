cee49d756e9599d47ecec5ce930af9918b37ba4f
Dudemanguy <random342@airmail.cc>
2023-10-01 01:19:40 -0500

    vo_dmabuf_wayland: eliminate an extra frame copy
    When implementing vo_dmabuf_wayland, it always did a copy of the image
    from the current frame and worked with that. The reason was because
    mpv's core held onto the frame and caused some timing issues and
    rendering glitches depending on when it freed the image. This is pretty
    easy to fix: just make vo_dmabuf_wayland manage the the frames. In vo.h,
    we add a boolean that a VO can set to make them manage freeing frames
    directly. After doing this, change the buffers in vo_dmabuf_wayland to
    store the whole vo_frame instead of just the image. Then, just modify
    some things a bit so frame is freed instead of the image. Now, we should
    truly have zero-copy playback. Well as long as you don't use libass to
    render anything (that's still a copy from system memory).

--- video/out/vo.c
+++ video/out/vo.c
@@ -967,7 +967,8 @@ static bool render_frame(struct vo *vo)
     pthread_cond_broadcast(&in->wakeup); // for vo_wait_frame()
 
 done:
-    talloc_free(frame);
+    if (!vo->driver->frame_owner)
+        talloc_free(frame);
     if (in->wakeup_on_done && !still_displaying(vo)) {
         in->wakeup_on_done = false;
         wakeup_core(vo);
@@ -1005,7 +1006,7 @@ static void do_redraw(struct vo *vo)
     vo->driver->draw_frame(vo, frame);
     vo->driver->flip_page(vo);
 
-    if (frame != &dummy)
+    if (frame != &dummy && !vo->driver->frame_owner)
         talloc_free(frame);
 }
 
