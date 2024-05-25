947a3f15ea14a2c19ae63b0b764bfaf04a223a28
Dudemanguy <random342@airmail.cc>
2024-05-25 14:46:54 -0500

    player: replace get_current_time with get_playback_time in some spots
    The problem with using get_current_time is that it can return negative
    values. Interally in mpv, we need this but for some spots negative
    values are not great. Since the previous commit enforced nonnegative
    returns for get_playback_time, we can use that instead. The two areas
    changed here are watch later configfile writing (clearly should always
    be a positive number) and the time-pos property (negative numbers are
    nonsense for users). This fully fixes #10346.

--- player/configfiles.c
+++ player/configfiles.c
@@ -305,7 +305,7 @@ void mp_write_watch_later_conf(struct MPContext *mpctx)
     write_filename(mpctx, file, cur->filename);
 
     bool write_start = true;
-    double pos = get_current_time(mpctx);
+    double pos = get_playback_time(mpctx);
 
     if ((demux && (!demux->seekable || demux->partially_seekable)) ||
         pos == MP_NOPTS_VALUE)
