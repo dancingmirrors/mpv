64f9f50dc145be1913fc817e2d31218f5723656a
Kacper Michajłow <kasper93@gmail.com>
2024-07-16 22:39:12 +0200

    player/command: don't return ill-formed node if there is no default val
    option-info/<name>/default-value would be initialized with zeroed
    object, which is not valid for string typed options, as the would be set
    to NULL.
    
    Found by OSS-Fuzz.

--- misc/json.c
+++ misc/json.c
@@ -254,6 +254,8 @@ static const char special_escape[] = {
 
 static void write_json_str(bstr *b, unsigned char *str)
 {
+    mp_assert(str);
+
     APPEND(b, "\"");
     while (1) {
         unsigned char *cur = str;
