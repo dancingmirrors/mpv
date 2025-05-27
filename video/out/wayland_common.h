d08a48a67ea8ca79867ab79dc4bd44097aac3889
dancingmirrors <dancingmirrors@icloud.com>
2025-05-27 17:53:28 -0600

    Make a few of the random headers consistent with the rest.

--- video/out/wayland_common.h
+++ video/out/wayland_common.h
@@ -1,5 +1,5 @@
 /*
- * This file is part of mpv video player.
+ * This file is part of mpv.
  *
  * mpv is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
