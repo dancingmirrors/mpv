58071c70146629a39854ef3bdbfcda65f22f261c
sfan5 <sfan5@live.de>
2025-08-21 16:54:47 +0200

    player/command: fix missing property update for some hwdec changes
    fixes #16710

--- player/command.c
+++ player/command.c
@@ -7087,6 +7087,9 @@ void mp_option_change_callback(void *ctx, struct m_config_option *co, int flags,
         struct mp_decoder_wrapper *dec = track ? track->dec : NULL;
         if (dec) {
             mp_decoder_wrapper_control(dec, VDCTRL_REINIT, NULL);
+            // filter chain might not change (which normally triggers this), but
+            // hwdec will.
+            mp_notify(mpctx, MPV_EVENT_VIDEO_RECONFIG, NULL);
             double last_pts = mpctx->video_pts;
             if (last_pts != MP_NOPTS_VALUE)
                 queue_seek(mpctx, MPSEEK_ABSOLUTE, last_pts, MPSEEK_EXACT, 0);
