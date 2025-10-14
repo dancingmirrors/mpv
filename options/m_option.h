68fc96efed81af13460e86c2c89a305909482675
dancingmirrors <dancingmirrors@icloud.com>
2025-10-14 12:40:16 -0600

    Cosmetic update to some definitions in m_option.h.

--- options/m_option.h
+++ options/m_option.h
@@ -456,16 +456,16 @@ char *format_file_size(int64_t size);
     (((UPDATE_OPT_LAST << 1) - 1) & ~(unsigned)(UPDATE_OPT_FIRST - 1))
 
 // type_float/type_double: string "default" is parsed as NaN (and reverse)
-#define M_OPT_DEFAULT_NAN       (1 << 25)
+#define M_OPT_DEFAULT_NAN       (1 << 24)
 
 // type time: string "no" maps to MP_NOPTS_VALUE (if unset, NOPTS is rejected)
-#define M_OPT_ALLOW_NO          (1 << 26)
+#define M_OPT_ALLOW_NO          (1 << 25)
 
 // type channels: disallow "auto" (still accept ""), limit list to at most 1 item.
-#define M_OPT_CHANNELS_LIMITED  (1 << 27)
+#define M_OPT_CHANNELS_LIMITED  (1 << 26)
 
 // Like M_OPT_TYPE_OPTIONAL_PARAM.
-#define M_OPT_OPTIONAL_PARAM    (1 << 30)
+#define M_OPT_OPTIONAL_PARAM    (1 << 27)
 
 // These are kept for compatibility with older code.
 #define CONF_NOCFG              M_OPT_NOCFG
