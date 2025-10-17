b4eea264fc72c2e8a172d173f99f8cbc9ad7a145
dancingmirrors <dancingmirrors@icloud.com>
2025-10-17 04:39:40 -0600

    Update a few comments in gpu/ra.

--- video/out/gpu/ra.h
+++ video/out/gpu/ra.h
@@ -143,8 +143,8 @@ struct ra_tex_params {
                             // be true depends on ra_format.linear_filter)
     bool src_repeat;        // if false, clamp texture coordinates to edge
                             // if true, repeat texture coordinates
-    bool non_normalized;    // XXX
-    bool external_oes;      // hack for GL_TEXTURE_EXTERNAL_OES idiocy
+    bool non_normalized;    // hack for GL_TEXTURE_RECTANGLE
+    bool external_oes;      // hack for GL_TEXTURE_EXTERNAL_OES
     // If non-NULL, the texture will be created with these contents. Using
     // this does *not* require setting host_mutable. Otherwise, the initial
     // data is undefined.
