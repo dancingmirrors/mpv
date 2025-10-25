90db170ed5e07763dfa9ed7baa5d1750b5cda3dd
nanahi <130121847+na-na-hi@users.noreply.github.com>
2025-10-25 10:59:57 -0400

    various: use mp_sleep_ns

--- audio/out/ao_alsa.c
+++ audio/out/ao_alsa.c
@@ -40,6 +40,7 @@
 #include "options/m_option.h"
 #include "common/msg.h"
 #include "osdep/endian.h"
+#include "osdep/timer.h"
 
 #include <alsa/asoundlib.h>
 
@@ -973,7 +974,7 @@ static bool recover_and_get_state(struct ao *ao, struct mp_pcm_state *state)
             if (err == -EAGAIN) {
                 // Cargo-cult from decades ago, with a cargo cult timeout.
                 MP_INFO(ao, "PCM resume EAGAIN - retrying.\n");
-                sleep(1);
+                mp_sleep_ns(MP_TIME_S_TO_NS(1));
                 continue;
             }
             if (err == -ENOSYS) {
